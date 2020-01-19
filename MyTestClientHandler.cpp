e0cb44beb8f4c309dfb9ce852ad0c0471d6f04a6
sshiraa <sshiraa@gmail.com>
2020-01-19 21:51:51 +0200

    server and client handler updates

---
+++ MyTestClientHandler.cpp
@@ -0,0 +1,5 @@
+//
+// Created by shira on 19.1.2020.
+//
+
+#include "MyTestClientHandler.h"
---
+++ MyTestClientHandler.h
@@ -0,0 +1,12 @@
+//
+// Created by shira on 19.1.2020.
+//
+
+#ifndef EX4__MYTESTCLIENTHANDLER_H_
+#define EX4__MYTESTCLIENTHANDLER_H_
+
+class MyTestClientHandler {
+
+};
+
+#endif //EX4__MYTESTCLIENTHANDLER_H_
